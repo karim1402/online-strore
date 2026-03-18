0bf761da04ce11250cd9012bfbab9128e867c0fb
karim <dev.karim12@gmail.com>
2026-03-18 14:27:30 +0200

    update the report

--- app/Http/Controllers/Api/User/AddressController.php
+++ app/Http/Controllers/Api/User/AddressController.php
@@ -464,7 +464,7 @@ class AddressController extends Controller
 
             return $this->successResponse([
                 'address_id'     => (int) $id,
-                'delivery_fees'  => 0.00, //$totalFee,
+                'delivery_fees'  => 0, //$totalFee,
                 'currency'       => 'EGP',
             ], 'success.data_retrieved');
         } catch (\Exception $e) {
