e234ff91dd2f37146ed2990bad3e8537611eca38
karim <dev.karim12@gmail.com>
2026-04-30 15:21:52 +0300

    commint the changes in the cart stock check

--- app/Http/Controllers/Api/User/OrderController.php
+++ app/Http/Controllers/Api/User/OrderController.php
@@ -246,6 +246,10 @@ class OrderController extends Controller
             foreach ($cart->items as $cartItem) {
                 $product = $cartItem->product;
 
+                // if ($product->module_id == 31 && $product->stock !== null) {
+                //     $product->decrement('stock', $cartItem->quantity);
+                // }
+
                 // Create product snapshot
                 $productSnapshot = [
                     'id' => $product->id,
@@ -560,6 +564,8 @@ class OrderController extends Controller
         $order->reason = $request->input('reason');
         $order->save();
 
+        // $order->restoreStock();
+
         // Log activity
         activity()
             ->performedOn($order)
@@ -737,6 +743,16 @@ class OrderController extends Controller
                 ];
                 continue;
             }
+
+            // Check stock for SME products (module 31)
+            // if ($product->module_id == 31 && $product->stock !== null) {
+            //     if ($product->stock < $item->quantity) {
+            //         $errors[] = [
+            //             'product_id' => $product->id,
+            //             'error' => "Product '{$product->name}' is out of stock",
+            //         ];
+            //     }
+            // }
 
             // Check store is approved
             // if ($product->store->status !== 'approved') {
