14daf0f9f7531fa106276b57df0e4c0a45e97721
karim <dev.karim12@gmail.com>
2026-04-30 14:35:59 +0300

    revert: remove SME stock tracking from User API

--- app/Http/Controllers/Api/User/OrderController.php
+++ app/Http/Controllers/Api/User/OrderController.php
@@ -246,10 +246,6 @@ class OrderController extends Controller
             foreach ($cart->items as $cartItem) {
                 $product = $cartItem->product;
 
-                if ($product->module_id == 31 && $product->stock !== null) {
-                    $product->decrement('stock', $cartItem->quantity);
-                }
-
                 // Create product snapshot
                 $productSnapshot = [
                     'id' => $product->id,
@@ -564,8 +560,6 @@ class OrderController extends Controller
         $order->reason = $request->input('reason');
         $order->save();
 
-        $order->restoreStock();
-
         // Log activity
         activity()
             ->performedOn($order)
@@ -742,16 +736,6 @@ class OrderController extends Controller
                     'error' => LocalizationService::getMessage('cart.product_unavailable'),
                 ];
                 continue;
-            }
-
-            // Check stock for SME products (module 31)
-            if ($product->module_id == 31 && $product->stock !== null) {
-                if ($product->stock < $item->quantity) {
-                    $errors[] = [
-                        'product_id' => $product->id,
-                        'error' => "Product '{$product->name}' is out of stock",
-                    ];
-                }
             }
 
             // Check store is approved
