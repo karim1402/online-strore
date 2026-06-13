838b9b604b44881a7aaf0710dd183138fef71fff
Kemo Mohamed <kemomac@MacBook-Air-Kemo.local>
2026-06-13 17:30:39 +0300

    fix

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -140,13 +140,15 @@ class UserAddress extends Model
     public function calculateDeliveryFee(float $subtotal = 0, $cartItems = null): float
     {
         // If cart contains restricted products, delivery fee is fixed at 10
-        if ($cartItems && \App\Models\Voucher::cartHasRestrictedProducts($cartItems)) {
-            return 10.00;
-        }
+       
 
         // Free delivery if subtotal is above threshold
         if ($subtotal > 149) {
             return 0.00;
+        }
+
+         if ($cartItems && \App\Models\Voucher::cartHasRestrictedProducts($cartItems)) {
+            return 10.00;
         }
 
         // Free delivery for the user's first order if subtotal is over 100
